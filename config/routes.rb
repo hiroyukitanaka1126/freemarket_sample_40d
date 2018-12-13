8f69e35bc825c84e8422f9161fc572155e4807c2
koyablue <koyaaoyama@KoyaAoyamanoMacBookPro.local>
2018-12-13 18:00:21 +0900

    edit routing
    購入確認画面表示するためにconfirmをroutingに追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root 'items#index'
-  resources :items
+  resources :items do
+    member do
+      get :confirm
+    end
+  end
 end
+
+#購入確認画面の表示のためconfirmを追加。itemのid拾うと思われるのでmemberで一旦書いてみる
