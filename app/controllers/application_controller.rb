f3354e7258bc38f2ebfa7f64e996c95c47c579d1
koyablue <koyaaoyama@KoyaAoyamanoMacBookPro.local>
2018-12-17 19:25:30 +0900

    edit Basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ before_action :basic_auth, if: :production?
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      user == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 
