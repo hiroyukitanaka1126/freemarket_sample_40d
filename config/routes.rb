c80f533f867327cda9602f511bff1bf4aafc0bbc
koyablue <koyaaoyama@KoyaAoyamanoMacBookPro.local>
2018-12-19 20:21:30 +0900

    新規登録画面(アカウント選択)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+
   devise_for :users
+
   root 'items#index'
+
   resources :items do
     member do
       get :confirm
+    end
+  end
+
+  resources :users do
+    collection do
+      get :signup
     end
   end
 end
