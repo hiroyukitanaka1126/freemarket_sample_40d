bbfe672e501f39c9a56ccfa4cfbe9152b3f52eea
koyablue <koyaaoyama@KoyaAoyamanoMacBookPro.local>
2018-12-21 14:58:02 +0900

    edit sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook]
 
   def self.from_omniauth(auth)
     user = User.where(email: auth.info.email).first
