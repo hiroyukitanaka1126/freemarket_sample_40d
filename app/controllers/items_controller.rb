7ff16d5e13af51be92ec3bf7afebe7a914161d97
SK2929 <sk@koyamashoutairanoMacBook-Pro.local>
2018-12-22 12:16:44 +0900

    Edit.items/new.html.haml

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,16 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -12,4 +22,11 @@ class ItemsController < ApplicationController
   def confirm
     #購入確認画面表示のためのアクション
   end
+
+private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,26 @@
 class Item < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 40 }      # 「40文字以下」
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300 }
+# numericality: :only_integer
+
+  def show_error_message_item(item)
+    if item.length == 0
+      "入力してください"
+    elsif item.length > 1000
+      "1000文字以下で入力してください"
+    elsif item.length > 40
+      "40文字以下で入力してください"
+    else
+      ""
+    end
+  end
+
+  def show_error_message_price(item)
+    if (item.length == 0) || (item.to_i > 299) == false
+      "300以上9999999以下で入力してください"
+    else
+      ""
+    end
+  end
 end
