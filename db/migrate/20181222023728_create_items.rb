e11431f0a32fff45b36d42b244674221381cd823
SK2929 <sk@koyamashoutairanoMacBook-Pro.local>
2018-12-22 11:42:39 +0900

    Create-itemテーブル

---
+++ db/migrate/20181222023728_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration[5.0]
+  def change
+    create_table :items do |t|
+      t.string :name, null: false, index: true
+      t.text :description, null: false
+      t.string :price, null: false
+      t.integer :likes_count
+      t.timestamps
+    end
+  end
+end
