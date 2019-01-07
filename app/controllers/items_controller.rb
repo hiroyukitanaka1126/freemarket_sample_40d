660f6526648267ef126691c20bc5165e00c5c0d0
SK2929 <sk@koyamashoutairanoMacBook-Pro.local>
2019-01-07 13:22:38 +0900

    Payjpで買われた商品にbuyer_idを付与する処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,10 +38,15 @@ class ItemsController < ApplicationController
   end
 
   def pay_jp
-    amount = Item.find(params[:id]).price
+    item = Item.find(params[:id])
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    Payjp::Charge.create(currency: "jpy", amount: amount, card: params["payjp-token"])
+    Payjp::Charge.create(currency: "jpy", amount: item[:price], card: params["payjp-token"])
+    pay_jp_item_buyer_id_update(item)
     redirect_to root_path, notice: "支払いが完了しました"
+  end
+
+  def pay_jp_item_buyer_id_update(item)
+    item.update( buyer_id: current_user.id)
   end
 
 
