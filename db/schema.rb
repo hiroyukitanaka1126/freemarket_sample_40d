a08f01a8f3aa57d1172d55252e40ef19e39edd30
koyablue <koyaaoyama@KoyaAoyamanoMacBookPro.local>
2019-01-07 18:16:11 +0900

    migrate user_id -> buyer_id

--- db/schema.rb
+++ db/schema.rb
@@ -49,8 +49,6 @@ ActiveRecord::Schema.define(version: 20190104055048) do
     t.datetime "updated_at",                null: false
     t.string   "image"
     t.integer  "user_id"
-    t.integer  "buyer_id"
-    t.index ["buyer_id"], name: "index_items_on_buyer_id", using: :btree
     t.index ["name"], name: "index_items_on_name", using: :btree
     t.index ["user_id"], name: "index_items_on_user_id", using: :btree
   end
