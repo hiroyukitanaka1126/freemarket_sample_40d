2e05f3252581ff0dc5b360927edcb7b898dc4444
koyablue <koyaaoyama@KoyaAoyamanoMacBookPro.local>
2019-01-08 18:52:14 +0900

    edit image_uploader and carrierwavr

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,5 +16,6 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1'
     }
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mercari40d'
+    config.cache_storage = :fog
   end
 end
