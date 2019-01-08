88ca134edcbfb335c653a94cf13a44f171ad45a3
koyablue <koyaaoyama@KoyaAoyamanoMacBookPro.local>
2019-01-08 16:36:13 +0900

    edit image_uploader and carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,13 +8,13 @@ CarrierWave.configure do |config|
   elsif Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory = 'chat-space-uploads'
+    config.fog_directory = '40d-mercari'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.secrets.aws_access_key_id,
       aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
       region: 'ap-northeast-1'
     }
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space-uploads'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/40d-mercari'
   end
 end
